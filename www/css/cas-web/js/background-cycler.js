eb0fca46f3cbf59c6f01de5a93fc2aa342571363
MJenkins2 <47362678+MJenkins2@users.noreply.github.com>
2019-03-28 15:27:53 -0400

    Add files via upload

---
+++ www/css/cas-web/js/background-cycler.js
@@ -0,0 +1,47 @@
+﻿$(function () {
+    function cyclerSize() {
+        var windowWidth = $(window).width();
+        if (windowWidth >= 1200) {
+            $(".cycler>img[src$='-md.jpg']").each(function () {
+                var new_src = $(this).attr("src").replace('-md', '-lg');
+                $(this).attr("src", new_src);
+            });
+            $(".cycler>img[src$='-sm.jpg']").each(function () {
+                var new_src = $(this).attr("src").replace('-sm', '-lg');
+                $(this).attr("src", new_src);
+            });
+        }
+        else if (windowWidth >= 992) {
+            $(".cycler>img[src$='-lg.jpg']").each(function () {
+                var new_src = $(this).attr("src").replace('-lg', '-md');
+                $(this).attr("src", new_src);
+            });
+            $(".cycler>img[src$='-sm.jpg']").each(function () {
+                var new_src = $(this).attr("src").replace('-sm', '-md');
+                $(this).attr("src", new_src);
+            });
+        }
+        else {
+            $(".cycler>img[src$='=-lg.jpg']").each(function () {
+                var new_src = $(this).attr("src").replace('-lg', '-sm');
+                $(this).attr("src", new_src);
+            });
+            $(".cycler>img[src$='-md.jpg']").each(function () {
+                var new_src = $(this).attr("src").replace('-md', '-sm');
+                $(this).attr("src", new_src);
+            });
+        }
+    };
+    cyclerSize();
+    $(window).resize(cyclerSize);
+    var fElement = $('.cycler');
+    fElement.find('img:gt(0)').hide();
+    setInterval(function () {
+        if (!fElement.data('paused')) {
+            fElement.find(':first-child').stop(true, true).fadeOut(1000);
+            fElement.find(':first-child').next('img').fadeIn(1000).end(1000).appendTo('.cycler'); //.stop(true,true) fixes le tabbed/hidden animation queue
+        } else {
+            console.log('waiting...');
+        }
+    }, 20000);
+});
---
+++ www/mapPage.js
@@ -0,0 +1,73 @@
+﻿(function (global) {
+
+    "use strict";
+
+
+
+    function onDeviceReady() {
+
+        document.addEventListener("online", onOnline, false);
+
+        document.addEventListener("resume", onResume, false);
+
+        loadMapsApi();
+
+    }
+
+
+
+    function onOnline() {
+
+        loadMapsApi();
+
+    }
+
+
+
+    function onResume() {
+
+        loadMapsApi();
+
+    }
+
+
+
+    function loadMapsApi() {
+
+        if (navigator.connection.type === Connection.NONE || (global.google !== undefined && global.google.maps)) {
+
+            return;
+
+        }
+
+
+
+        //TODO: Add your own Google maps API key to the URL below.
+
+        $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyD-c1rMH_fmgjuayaFXerh_NZGdrPXVSA4&https://maps.googleapis.com/maps/api/js?key=AIzaSyD-c1rMH_fmgjuayaFXerh_NZGdrPXVSA4&callback=onMapsApiLoaded');
+
+    }
+
+
+
+    global.onMapsApiLoaded = function () {
+
+        // Maps API loaded and ready to be used.
+
+        var map = new google.maps.Map(document.getElementById("map"), {
+
+            zoom: 8,
+
+            center: new google.maps.LatLng(-34.397, 150.644)
+
+        });
+
+    };
+
+
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+})(window);
+
+key = AIzaSyD - c1rMH_fmgjuayaFXerh_NZGdrPXVSA4
